f74abd734eec4021c92d766a448668814db77643
Simppa <simo.siren@careeria.fi>
2025-09-26 14:26:17 +0300

    2 pv valmis

--- ShoppingApp/MainPage.xaml.cs
+++ ShoppingApp/MainPage.xaml.cs
@@ -1,12 +1,37 @@
-﻿namespace ShoppingApp
+﻿using ShoppingApp.Models;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+
+namespace ShoppingApp
 {
     public partial class MainPage : ContentPage
     {
-
         public MainPage()
         {
             InitializeComponent();
+            LoadDataFromRestAPI();
         }
 
+        // LISTAN HAKEMINEN BACKENDISTÄ
+        public async Task LoadDataFromRestAPI()
+        {
+            // Latausilmoitus näkyviin
+            Loading_label.IsVisible = true;
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://shoppingbackendope.azurewebsites.net");
+            string json = await client.GetStringAsync("/api/shoplist/");
+
+            // Deserialisoidaan json muodosta C# muotoon
+            IEnumerable<Shoplist> ienumShoplist = JsonConvert.DeserializeObject<Shoplist[]>(json);
+
+         
+            ObservableCollection<Shoplist> shoplistCollection = new(ienumShoplist);
+
+            // Data näkyviin listassa
+            itemList.ItemsSource = shoplistCollection;
+
+            // Latausilmoitus pois näkyvistä
+            Loading_label.IsVisible = false;
+        }
     }
 }
---
+++ ShoppingApp/Models/Shoplist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingApp.Models;
+
+public partial class Shoplist
+{
+    public int Id { get; set; }
+
+    public string Item { get; set; } = null!;
+
+    public int Amount { get; set; }
+}
